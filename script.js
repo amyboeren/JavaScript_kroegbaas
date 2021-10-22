c4ccc762af44ad6139acc113bf23a116eb20ae4a
AmyB <31877717+amyboeren@users.noreply.github.com>
2021-10-22 22:24:17 +0200

    Add files via upload
    Goedendag, ik begrijp niet waarom de if (driverstatus="bob") niet werkt. Kan ik hier feedback op krijgen?

---
+++ script.js
@@ -0,0 +1,30 @@
+const age=19;
+console.log(age);
+
+//If-Else
+if (age>=18) {
+    document.write("Je bent 18 jaar of ouder en mag naar binnen");
+}
+else {
+    document.write("Je bent jonger dan 18 jaar en mag niet naar binnen");
+}
+
+const isFemale=true;
+console.log(isFemale)
+
+if (isFemale) {
+    document.write("Je bent een vrouw");
+}
+else {
+    document.write("Je bent een man");
+}
+
+const driverStatus="bob";
+console.log(driverStatus);
+
+if (driverStatus="bob") {
+    document.write("Je mag rijden");
+}
+else {
+    document.write("Je mag helaas niet meer rijden");
+}
